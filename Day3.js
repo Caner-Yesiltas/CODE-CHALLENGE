be7f89793e39a18c8ae52f373823561bb50c70a0
CanerYesiltas <caneryesiltas1@gmail.com>
2024-07-05 16:34:54 -0400

    Finally started to solve loops idea

--- Day3.js
+++ Day3.js
@@ -78,9 +78,21 @@ console.log(`The bonus based on the performance score is: ${bonus}`);
 
 Question 14
 
+Write a for loop that prints numbers from 1 to 5.
+
+*/
+
+let index=1
 
 
+for (let i=1; i<=5; i++) {
+    console.log(i);
+}
 
+/*
 
+Question 15
+
+Write a for loop that prints even numbers from 0 to 10.
 
 */
