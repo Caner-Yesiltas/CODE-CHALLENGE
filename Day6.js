05fc7a1ea7f62c6b3968e84077516ecc684a4927
CanerYesiltas <caneryesiltas1@gmail.com>
2024-07-08 16:35:26 -0400

    Some Basic Functions  with if, loops, parameters

--- Day6.js
+++ Day6.js
@@ -1,6 +1,107 @@
- /*
+/*
 Question 38
 
+Write a function named concatenate that takes two parameters, str1 and str2, and returns a single string that is the concatenation of str1 and str2.
+
+*/
+
+function concatenate(str1, str2) {
+  result = str1 + str2;
+  return result;
+}
+
+console.log(concatenate("jin", "Woo"));
+
+/*
+Question 39
+
+Write a function that takes two numbers and returns their sum
 
 
 */
+
+function caner(num1, num2) {
+  let sum = num1 + num2;
+  return sum;
+}
+
+console.log(caner(1, 2));
+
+
+/*
+
+Question 40
+
+Write a function that converts each element in an array to a string
+
+
+*/
+
+function convertToString(arr) {
+
+  return arr.map(string);
+    
+}
+
+console.log(convertToString([1,2,3]));
+
+
+/*
+
+Question 41
+
+ Write a function that checks if a number is positive
+
+
+*/
+
+
+function checkPositiveNumber(number) {
+
+    let result;
+
+    if (number>0) {
+        result="Positive"; }
+
+
+        else if (number<0) {
+            result="Negative";
+        }
+
+        else {result="Notr";}
+    
+    
+    return result
+
+}
+
+ console.log(checkPositiveNumber(-5)); 
+
+
+ 
+/*
+
+Question 41
+
+ Write a function that determines if a number is even or odd
+
+
+*/
+
+
+function detectEvenOrOddNumbers(Number2) {
+
+    let result2
+
+ if (Number2 % 2 === 0) { return "Even";
+    
+ } else { return "Odd";
+    
+ }
+
+    
+}
+
+console.log(detectEvenOrOddNumbers(7)); 
+
+
