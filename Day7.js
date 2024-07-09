af2dca113e1059f8afe5ac70f52e249cb42cd394
CanerYesiltas <caneryesiltas1@gmail.com>
2024-07-09 11:25:41 -0400

    Fibonacci and Recursion Function Added

--- Day7.js
+++ Day7.js
@@ -21,10 +21,39 @@ function factorial(n) {
 
 console.log(factorial(5));
 
+const factorial2 = (n) => {
+  if (n === 0 || n === 1) {
+    return 1;
+  } else {
+    return n * factorial2(n - 1);
+  }
+};
+
+console.log(factorial2(5)); // arrow function ile yapilandirilmasi second way !
+
 /*
 
 Question 46
 
+ Fibonacci dizisi, her sayının kendisinden önce gelen iki sayının toplamı olduğu bir sayı dizisidir. Dizi genellikle 0 ve 1 ile başlar.
 
+Fibonacci Dizisi Sorusu:
+Soru: Fibonacci dizisinin n. elemanını bulan bir fonksiyon yazın. Fibonacci dizisi şu şekilde tanımlanır:
+
+F(0) = 0
+F(1) = 1
+F(n) = F(n-1) + F(n-2) (n > 1 için)
 
 */
+
+function fibonacci(n) {
+  if (n === 0) {
+    return 0;
+  } else if (n === 1) {
+    return 1;
+  } else {
+    return fibonacci(n - 1) + fibonacci(n - 2);
+  }
+}
+
+console.log(fibonacci(5));
