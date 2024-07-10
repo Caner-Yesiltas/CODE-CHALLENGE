757423d9c9211d925d6a7ae7be33dcfbc322fbc2
CanerYesiltas <caneryesiltas1@gmail.com>
2024-07-10 18:33:39 -0400

    Basic String Methods Examples

--- Day8.js
+++ Day8.js
@@ -2,6 +2,46 @@
 
 Question 47
 
-
+Convert all the letters of a string to uppercase.
 
 */
+
+
+const anime ="solo leveling"
+const convertToAnime = anime.toUpperCase();
+console.log(convertToAnime);
+
+const allSmallLetters= convertToAnime.toLowerCase();
+console.log(allSmallLetters);
+
+
+/*
+
+Question 48
+
+Find the character at a specific index in a string.
+
+*/
+
+const forLove="lOVE IS LOVE "
+const findLove=forLove.charAt(6);
+const findLoveEnd=forLove.charAt(13);
+
+console.log(findLove);
+console.log(findLoveEnd);
+
+/*
+
+Question 49
+
+Check if a string contains a specific substring.
+
+*/
+
+const fullstack="react, javascript, HTML"
+const findLibrary=fullstack.includes("react");
+console.log(findLibrary);
+
+
+
+
