c50c3062424fa2ab742e75f42e881ba063d673b6
CanerYesiltas <caneryesiltas1@gmail.com>
2024-07-12 23:28:39 -0400

    End of days10

--- Day10.js
+++ Day10.js
@@ -68,7 +68,56 @@ iterateString(str);
 
 /*
 
-Question 63  
+Question 63  INTERVIEW QUESTION
 
+Bir string içindeki tüm boşlukları '%20' ile değiştiren bir fonksiyon yazın.
 
 */
+
+function replaceSpaces(str) {
+
+  let splitstr= str.split(' '); // bosluk kelime kelime ayirir demek
+
+  let result=splitstr.join('%20'); // joinin icine ne yazarsan kelimelerin arasina onlari koyar iki kelimenin arasina ve sadece dizilerde kullanilir.
+
+  return result
+  
+}
+
+
+console.log(replaceSpaces("Hello World")); // "Hello%20World"
+console.log(replaceSpaces("Front end developer")); // "Front%20end%20developer"
+
+
+
+/*
+
+Question 64  INTERVIEW QUESTION
+
+Verilen bir sayı dizisindeki en büyük ve en küçük sayıyı bulup farkını döndüren bir fonksiyon yazın. 
+(Diziler ve objeler konularına girmemek için string ve basit matematik kullanabilirsiniz.)
+
+*/
+
+function findDifference(numbers) {
+
+let numArray = number.split(' '); // stringi bosluk karakterine gore boler ve sonra her birini dizi haline getirir.
+
+numArray= numArray.map(Number); // Dizi deki elemanlari sayiya donusturur.
+
+
+let maxNum= math.max(...numArray);
+
+let minNum=math.min(...numArray);
+
+let difference= maxNum-minNum;
+
+return difference;
+
+}
+  
+
+
+
+console.log(findDifference("1 2 3 4 5")); // 4 (5 - 1)
+console.log(findDifference("10 20 30 40 50")); // 40 (50 - 10)
