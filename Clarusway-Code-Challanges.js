1584320bccf3e986361d0f0f027ef4b8903cd348
CanerYesiltas <caneryesiltas1@gmail.com>
2024-07-16 18:01:03 -0400

    try to make 2. way palindrome question with for

--- Clarusway-Code-Challanges.js
+++ Clarusway-Code-Challanges.js
@@ -61,8 +61,58 @@ console.log(reverseStringWithFor("Uber"));
 
 /*
 
-Question 1
+Question 2
 
+JS-CC-02 : Check Palindrome
+Purpose of the this coding challenge is to write a code that checks whether a string is palindromic or not.
 
+What is palindrome?
+
+A palindrome is a word, number, phrase, or other sequence of symbols that reads the same backwards as forwards, such as the words madam or racecar, and the sentence: "A man, a plan, a canal – Panama".
+
+Learning Outcomes
+At the end of the this coding challenge, students will be able to;
+
+analyze a problem, identify and apply programming knowledge for appropriate solution.
+
+demonstrate their knowledge of algorithmic design principles by using JavaScript effectively.
+
+Problem Statement
+Take an input from user.
+
+Check that input whether it is palindromic or not:
+
+if palindromic then console log that it is palindromic
+
+if not palindromic then console log that it is not palindromic
+
+⌛ Happy Coding ✍
 
 */
+
+function checkPalindrome(str) {
+
+    let reverseStr2=""
+
+    reverseStr2=str.split('').reverse().join('');
+
+    if(reverseStr2=== str) {
+        console.log("it is palindromic");
+    } else {
+        console.log("It is not palindromic");
+    }
+    
+    return reverseStr2; // degeri geri dondurur eger bos birakirsak returnsiz undefined der
+}
+
+console.log(checkPalindrome("SAS"));
+
+
+
+function checkPalindromeWithFor(str) {
+
+const len = str.length; // str.length tekrar yazmamak icin leb diye bir degisken atanmistir
+
+for(   )
+    
+}
